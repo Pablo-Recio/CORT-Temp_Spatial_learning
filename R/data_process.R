a427c2f84e06cf3de4c1f623180ef150e2c8425f
Pablo-Recio <93.pablo.recio@gmail.com>
2025-03-03 10:37:30 +1100

    updates

--- R/data_process.R
+++ R/data_process.R
@@ -5,8 +5,8 @@ pacman::p_load(tidyverse, flextable, emmeans, DHARMa, brms, here, ggplot2, lme4,
 #
 # A) PROCESSING THE LEARNING DF
 data <- read.csv(here("./data/Spatial_learn.csv"))
-#### A.1) Remove individuals who did not participate (more than 15 NAs), remove trials [36-40] (Only in associative) and split treatment into Temp and Cort
-data_spal <- data %>%
+#### A.1) Modify the learning df
+learning_df <- 
   group_by(lizard_id) %>%
     filter(sum(is.na(choice)) <= 15) %>%
   ungroup()  %>%
@@ -18,37 +18,27 @@ data_spal <- data %>%
     mutate(cort = factor(cort,
       levels = c("B", "A"),
       labels = c("B" = "CORT", "A" = "Control"))) %>%
-  ungroup() %>% 
+  ungroup() %>%
 data.frame()
-#
+write.csv(learning_df, here("./output/databases_clean/learning_df.csv"))
 #
 #### A.2) Extract the slope of each individual
 #
-## Fit the models only if they have not been fit yet (if refit = TRUE)
-if(refit==TRUE){
-  # Choice
-  model_choice <- brm(choice ~ 1 + (1 + day|lizard_id) + (1|clutch),
-                data = data_spal,
-                family = bernoulli(link = "logit"),
-                chains = 4, cores = 4, iter = 8000, warmup = 2000, 
-                control = list(adapt_delta = 0.99, max_treedepth = 12))
-  # Errors
-  model_errors <- brm(errors ~ 1 + (1 + day|lizard_id) + (1|clutch),
+## Fit the model only if it has not been fit yet (if refit = TRUE)
+if(refit){
+  model_errors <- brm(errors ~ 1 + (1 + day|lizard_id),
                 data = data_spal,
                 negbinomial(link = "log"),
                 chains = 4, cores = 4, iter = 8000, warmup = 2000,
                 control = list(adapt_delta = 0.99, max_treedepth = 12))
-  # Write the models to two files
-  saveRDS(model_choice, file = paste0(here("output/models/model_choice.rds")))
+  # Write the model to rds files
   saveRDS(model_errors, file = paste0(here("output/models/model_errors.rds")))
   } else {
     # Read the model from a file
-    model_choice <- readRDS(file = paste0(here("output/models/model_choice.rds")))
     model_errors <- readRDS(file = paste0(here("output/models/model_errors.rds")))
 } 
 #
 ## Extract posteriors
-posteriors_choice <- as_draws_df(model_choice)
 posteriors_errors <- as_draws_df(model_errors)
 #
 #
@@ -75,7 +65,7 @@ learning_df <- merge(mod_spal, learning_posteriors, by = "lizard_id")
 #
 #
 #
-# B) PROCESSING THE PHYSIOLOGY DF
+# B) PROCESSING THE PHYSIOLOGY DFs
 #### B.1) Modify the mit_df ("./data/mit_data.csv") and merge with the ()
 mit_data <- read.csv(here("./data/mit_data.csv"))
 mit_df <- mit_data %>%
